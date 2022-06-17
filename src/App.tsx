762c49d033e91dc1cd4dd211af466ab8d90f682f
Nate Butler <iamnbutler@gmail.com>
2022-06-17 01:39:42 -0400

    Add types, use strict mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,43 @@
 import * as React from 'react';
-import { Page, Rectangle, Text } from 'react-figma';
+import { Page, Rectangle, Text, Frame } from 'react-figma';
+
+const contextMenuItems = [
+  { label: "New Folder" },
+  { label: "New File" },
+  { label: "Copy", shortcut: "⌘C" },
+  { label: "Cut", shortcut: "⌘X" },
+  { label: "Copy Path", shortcut: "⌘⇧C" },
+  { label: "Copy Relative Path", shortcut: "⌘⌥C" },
+  { label: "Rename" },
+  { label: "Delete", shortcut: "⌘⌫", warn: true },
+]
 
 export const App = () => {
+  let menuItems = contextMenuItems.map((item) => {
     return (
-        <Page isCurrent name="Page X">
-            <Text characters="Hello, react-figma!"/>
-            <Rectangle style={{ width: 200, height: 100, backgroundColor: '#0ddd25' }} />
-            <Rectangle style={{ width: 200, height: 100, backgroundColor: '#a6dd00' }} />
-        </Page>
-    );
+      <Frame
+        name="ContextMenuItem"
+        style={{}}
+        layoutMode={'HORIZONTAL'}
+        primaryAxisAlignItems="SPACE_BETWEEN"
+        primaryAxisSizingMode="AUTO"
+      >
+        <Text name={item.label} characters={item.label} style={{ color: "#8B8792", fontSize: 14, fontFamily: "Fira Code" }} />
+        <Text name={item.shortcut} characters={item.shortcut} style={{ color: "#8B8792", fontSize: 14, fontFamily: "Fira Code" }} />
+      </Frame>
+    )
+  })
+
+  return (
+    <Page isCurrent name="ContextMenu">
+      <Frame
+        name="ContextMenu"
+        style={{ width: 160, height: 100, backgroundColor: "#26232A" }}
+        layoutMode={'VERTICAL'}
+        itemSpacing={2}
+      >
+        {menuItems}
+      </Frame>
+    </Page>
+  );
 };
